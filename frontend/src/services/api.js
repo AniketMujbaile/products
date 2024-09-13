042bed0587e5a971a0e84934c80757ab88a85757
Aniket Mujbaile <aniketmujbaile1@gmail.com>
2024-09-13 10:37:50 +0530

    Updated Hosted API_URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //const API_URL = 'http://localhost:5000/api';
-const API_URL = 'https://products-seven-ruddy.vercel.app/api';
+const API_URL = 'https://products-8bwd.onrender.com/api';
 
 export const getProducts = async (category = '') => {
   try {
